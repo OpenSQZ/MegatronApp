9ae6dd0ed63961294e07625d0a2c7a6e1f5acdea
liurt1218 <rliu0677@gmail.com>
2025-06-13 04:35:23 +0000

    megatron new version

---
+++ examples/gpt3/train_gpt3_175b_distributed.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Runs the "175B" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+GPUS_PER_NODE=4
+# Change for multinode config
+MASTER_ADDR=localhost
+MASTER_PORT=6000
+NUM_NODES=1
+NODE_RANK=0
+WORLD_SIZE=$(($GPUS_PER_NODE * $NUM_NODES))
+
+CHECKPOINT_PATH=ngc_models                                   #<Specify path>
+TENSORBOARD_LOGS_PATH=tensor_board                           #<Specify path>
+VOCAB_FILE=datasets/vocab.json                               #<Specify path to file>/gpt2-vocab.json
+MERGE_FILE=datasets/merges.txt                               #<Specify path to file>/gpt2-merges.txt
+DATA_PATH=datasets/gpt-large-cased-vocab-small_text_document #<Specify path and file prefix>_text_document
+
+DISTRIBUTED_ARGS=(
+    --nproc_per_node $GPUS_PER_NODE
+    --nnodes $NUM_NODES
+    --master_addr $MASTER_ADDR
+    --master_port $MASTER_PORT
+)
+
+GPT_MODEL_ARGS=(
+    --num-layers 32
+    --hidden-size 2048
+    --num-attention-heads 32
+    --seq-length 2048
+    --max-position-embeddings 2048
+    --attention-backend auto # Can use (flash/fused/unfused/local)
+)
+
+TRAINING_ARGS=(
+    --micro-batch-size 2
+    --global-batch-size 8
+    # --rampup-batch-size 16 16 5859375
+    --train-iters 20
+    --weight-decay 0.1
+    --adam-beta1 0.9
+    --adam-beta2 0.95
+    --init-method-std 0.006
+    --clip-grad 1.0
+    --fp16
+    --lr 6.0e-5
+    --lr-decay-style cosine
+    --min-lr 6.0e-6
+    --lr-warmup-fraction .001
+    --lr-decay-iters 430000
+    --use-app
+)
+
+MODEL_PARALLEL_ARGS=(
+    --tensor-model-parallel-size 1
+    --pipeline-model-parallel-size 4
+    --num-layers-per-virtual-pipeline-stage 2
+)
+
+DATA_ARGS=(
+    --data-path $DATA_PATH
+    --vocab-file $VOCAB_FILE
+    --merge-file $MERGE_FILE
+    --split 949,50,1
+)
+
+EVAL_AND_LOGGING_ARGS=(
+    --log-interval 1
+    --save-interval 50
+    --eval-interval 20
+    --save $CHECKPOINT_PATH
+    --load $CHECKPOINT_PATH
+    --eval-iters 10
+    --tensorboard-dir $TENSORBOARD_LOGS_PATH
+)
+
+torchrun ${DISTRIBUTED_ARGS[@]} pretrain_gpt.py \
+    ${GPT_MODEL_ARGS[@]} \
+    ${TRAINING_ARGS[@]} \
+    ${MODEL_PARALLEL_ARGS[@]} \
+    ${DATA_ARGS[@]} \
+    ${EVAL_AND_LOGGING_ARGS[@]}
